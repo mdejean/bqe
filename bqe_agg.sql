ae92824047794fdfc572836ad57869d416d5db2d
Marcel Dejean <marcel@dejean.nyc>
2022-05-22 11:27:16 -0400

    initial commit

---
+++ bqe_agg.sql
@@ -0,0 +1,73 @@
+with ls as
+(
+    select
+        link_id,
+        speed,
+        travel_time,
+        to_char(data_as_of, 'HH24') h,
+        to_char(data_as_of, 'YYYY-MM') ym,
+        to_char(data_as_of, 'DD') d,
+        case when extract(dow from data_as_of) in (0, 6) then 1 else 0 end weekend
+    from link_speed
+    where status = 0 
+    and data_as_of > '2018-01-01'
+)
+select
+    link_name,
+    quartiles.weekend,
+    quartiles.h,
+    quartiles.ym,
+    speed_worst,
+    travel_worst,
+    quartiles.speed[1] speed_25,
+    quartiles.speed[2] speed_50,
+    quartiles.speed[3] speed_75,
+    quartiles.travel_time[1] travel_25,
+    quartiles.travel_time[2] travel_50,
+    quartiles.travel_time[3] travel_75
+from link l
+join (
+    select
+        link_id,
+        percentile_cont(array[0.25, 0.5, 0.75]) within group (order by speed) speed,
+        percentile_cont(array[0.25, 0.5, 0.75]) within group (order by travel_time) travel_time,
+        weekend,
+        h,
+        ym
+    from ls
+    group by link_id, weekend, h, ym
+) quartiles on quartiles.link_id = l.link_id
+join (
+    select
+        link_id,
+        weekend,
+        h,
+        ym,
+        min(speed) speed_worst,
+        max(travel_time) travel_worst
+    from (
+        select 
+            link_id,
+            round(avg(speed)) speed,
+            round(avg(travel_time)) travel_time,
+            weekend,
+            h,
+            ym,
+            d
+        from ls 
+        group by link_id, weekend, h, ym, d
+    ) s
+    group by link_id, weekend, h, ym
+) hourly_avg on hourly_avg.link_id = l.link_id
+and quartiles.weekend = hourly_avg.weekend
+and quartiles.h = hourly_avg.h
+and quartiles.ym = hourly_avg.ym
+where link_name in (
+          'GOW N 9TH STREET - ATLANTIC AVENUE',
+          'BQE S LEONARD STREET - ATLANTIC AVENUE',
+          'BQE N ATLANTIC AVENUE - LEONARD STREET',
+          'BQE S - GOW S ALTANTIC AVENUE - 9TH STREET',
+          'BQE N Atlantic Ave - BKN Bridge Manhattan Side',
+          'BQE N Atlantic Ave - MAN Bridge Manhattan Side'
+          )
+order by link_name, quartiles.ym desc, quartiles.weekend, quartiles.h;
---
+++ bqe_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+last_d=`psql crashes --tuples-only -A -c "select to_char(max(data_as_of), 'YYYY-MM-DD\"T\"HH24:MI:SS') from link_speed"`
+curl 'https://data.cityofnewyork.us/resource/i4gi-tjb9.csv?$limit=1000000000&$select=link_id,data_as_of,status,speed,travel_time&$where=data_as_of>"'$last_d'"' --output link_speed.csv
+ogr2ogr -f "PostgreSQL" PG:"dbname=crashes" -append -nln link_speed_import -oo AUTODETECT_TYPE=YES link_speed.csv
+psql crashes --csv -f bqe_agg.sql -o bqe_agg.csv
